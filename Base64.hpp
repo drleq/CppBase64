c86af9fb66f85b69de49b572ee8de50d2142bced
Andrew Condie <condie@hotmail.com>
2019-02-20 23:52:16 +0000

    Do the thing better

--- Base64.hpp
+++ Base64.hpp
@@ -7,7 +7,25 @@
 namespace base64 {
 
     namespace detail {
-        constexpr std::string_view Base64Values{ "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" };
+        constexpr std::string_view Base64LUT{ "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" };
+        constexpr std::array<uint8_t, 256> Base64InverseLUT = {
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0x00 - 0x0F
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0x10 - 0x1F
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 62,  0,  0,  0, 63, // 0x20 - 0x2F
+            52, 53, 54, 55, 56, 57, 58, 59, 60, 61,  0,  0,  0,  0,  0,  0, // 0x30 - 0x3F
+             0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, // 0x40 - 0x4F
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  0,  0,  0,  0,  0, // 0x50 - 0x5F
+             0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, // 0x60 - 0x6F
+            41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,  0,  0,  0,  0,  0, // 0x70 - 0x7F
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0x80 - 0x8F
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0x90 - 0x9F
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0xA0 - 0xAF
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0xB0 - 0xBF
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0xC0 - 0xCF
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0xD0 - 0xDF
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, // 0xE0 - 0xEF
+             0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0  // 0xF0 - 0xFF
+        };
     }
 
     //--------------------------------------------------------------------------------------------------------
@@ -58,27 +76,26 @@ namespace base64 {
 
         size_t octet_end = (octet_count * 3);
         for (size_t i = 0; i < octet_end; i += 3) {
-            uint32_t octet =
-                (static_cast<uint32_t>(data[i  ]) << 16) |
-                (static_cast<uint32_t>(data[i+1]) << 8) |
-                (static_cast<uint32_t>(data[i+2]));
+            uint8_t b0 = static_cast<uint8_t>(data[i  ]);
+            uint8_t b1 = static_cast<uint8_t>(data[i+1]);
+            uint8_t b2 = static_cast<uint8_t>(data[i+2]);
 
-            uint32_t quad = detail::Base64Values[(octet      ) & 0x3F] << 24;
-            quad |= detail::Base64Values[(octet >>= 6) & 0x3F] << 16;
-            quad |= detail::Base64Values[(octet >>= 6) & 0x3F] << 8;
-            quad |= detail::Base64Values[(octet >>= 6)];
+            uint32_t quad = static_cast<uint32_t>(detail::Base64LUT[b0 >> 2]);
+            quad |= static_cast<uint32_t>(detail::Base64LUT[(b0 & 0x03) << 4 | b1 >> 4]) << 8;
+            quad |= static_cast<uint32_t>(detail::Base64LUT[(b1 & 0x0F) << 2 | b2 >> 6]) << 16;
+            quad |= static_cast<uint32_t>(detail::Base64LUT[b2 & 0x3F]) << 24;
 
             quad_callback(quad);
         }
 
         size_t remainder = data.size() - octet_end;
         if (remainder == 2) {
-            uint8_t b0 = static_cast<uint8_t>(data[octet_end]);
+            uint8_t b0 = static_cast<uint8_t>(data[octet_end  ]);
             uint8_t b1 = static_cast<uint8_t>(data[octet_end+1]);
 
-            byte_callback(detail::Base64Values[b0 & 0x3F]);
-            byte_callback(detail::Base64Values[(b0 >> 6) | (b1 & 0x0F) << 2]);
-            byte_callback(detail::Base64Values[b1 >> 2]);
+            byte_callback(detail::Base64LUT[b0 >> 2]);
+            byte_callback(detail::Base64LUT[(b0 & 0x03) << 4 | b1 >> 4]);
+            byte_callback(detail::Base64LUT[(b1 & 0x0F) << 2]);
             if (padded) {
                 byte_callback('=');
             }
@@ -86,8 +103,8 @@ namespace base64 {
         } else if (remainder == 1) {
             uint8_t b0 = static_cast<uint8_t>(data[octet_end]);
             
-            byte_callback(detail::Base64Values[b0 && 0x3F]);
-            byte_callback(detail::Base64Values[b0 >> 6]);
+            byte_callback(detail::Base64LUT[b0 >> 2]);
+            byte_callback(detail::Base64LUT[(b0 & 0x03) << 4]);
 
             if (padded) {
                 byte_callback('=');
@@ -149,18 +166,38 @@ namespace base64 {
         TTripleCallback triple_callback,
         [[maybe_unused]] TByteCallback byte_callback
     ) {
-        size_t octet_count = (data.size() / 4);
+        size_t binary_length = get_binary_length(data);
+        size_t octet_count = binary_length / 3;
 
         size_t octet_end = (octet_count * 4);
         for (size_t i = 0; i < octet_end; i += 4) {
-            uint8_t b0 = static_cast<uint8_t>(data[i  ]      | data[i+1] << 6);
-            uint8_t b1 = static_cast<uint8_t>(data[i+1] >> 2 | data[i+2] << 4);
-            uint8_t b2 = static_cast<uint8_t>(data[i+2] >> 4 | data[i+3] << 2);
+            uint8_t b0 = detail::Base64InverseLUT[data[i  ]];
+            uint8_t b1 = detail::Base64InverseLUT[data[i+1]];
+            uint8_t b2 = detail::Base64InverseLUT[data[i+2]];
+            uint8_t b3 = detail::Base64InverseLUT[data[i+3]];
 
-            triple_callback(b0, b1, b2);
+            uint8_t c0 = b0 << 2 | b1 >> 4;
+            uint8_t c1 = b1 << 4 | b2 >> 2;
+            uint8_t c2 = b2 << 6 | b3;
+
+            triple_callback(c0, c1, c2);
         }
 
+        size_t remainder = binary_length - (octet_count * 3);
+        if (remainder == 2) {
+            uint8_t b0 = detail::Base64InverseLUT[data[octet_end  ]];
+            uint8_t b1 = detail::Base64InverseLUT[data[octet_end+1]];
+            uint8_t b2 = detail::Base64InverseLUT[data[octet_end+2]];
 
+            byte_callback(b0 << 2 | b1 >> 4);
+            byte_callback(b1 << 4 | b2 >> 2);
+
+        } else if (remainder == 1) {
+            uint8_t b0 = detail::Base64InverseLUT[data[octet_end  ]];
+            uint8_t b1 = detail::Base64InverseLUT[data[octet_end+1]];
+
+            byte_callback(b0 << 2 | b1 >> 4);
+        }
     }
 
     //--------------------------------------------------------------------------------------------------------
--- Tests/Base64Test.cpp
+++ Tests/Base64Test.cpp
@@ -6,7 +6,7 @@ namespace {
 }
 
 TEST_CASE(Base64Test, Simple) {
-    const std::string Input = "Hello world";
+    const std::string Input = "Hello world.";
 
     auto encoded = base64::encode_to_string(Input);
     auto decoded = base64::decode_to_string(encoded);
